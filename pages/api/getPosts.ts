d77ca16cfc6ba3ae7c92041ea13aa56a08638c9e
Cormac Pujals <110591827+cormacpujals@users.noreply.github.com>
2023-02-19 22:36:17 -0800

    deployment config test

--- pages/api/getPosts.ts
+++ pages/api/getPosts.ts
@@ -9,10 +9,12 @@ export default async function handler(
   if (req.method === 'GET') {
     try {
       // get prisma to fetch the posts
-      const data = await prisma.post.findMany()
-      return res.status(200).json(data)
+      const data = await prisma.post.findMany();
+      console.log(JSON.stringify(data));
+      return res.status(200).json(data);
     } catch (error) {
-     return res.status(500).json(error) 
+      console.log(error);
+      return res.status(500).json(error);
     }
   }
 }
