f8b28efabc3394abdf535c598071ada3ab6a0fe6
Cormac Pujals <110591827+cormacpujals@users.noreply.github.com>
2023-02-20 00:30:50 -0800

    deployment config test

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,15 @@
 import Link from "next/link"
 import Form from "./Form";
 
-// return [{"id":1,"title":"Cats","content":"Cats are the best ","published":false},{"id":3,"title":"Dogs","content":null,"published":false},{"id":4,"title":"test","content":null,"published":false},{"id":5,"title":"working?","content":null,"published":false},{"id":6,"title":"test","content":null,"published":false},{"id":7,"title":"another test 4 u","content":null,"published":false}];
 async function getPosts() {
   // const url = `https://${process.env.VERCEL_URL}/api/getPosts`;
   let url = `https://experiment-next-prisma-tailwind.vercel.app/api/getPosts`;
-  try {
-    const res = await fetch(url);
-    if(!res.ok) {
-      console.log(`ERROR: fetch ${url} -> result not ok: |${JSON.stringify(res)}|`);
-      return [{id: 1, title: 'res.ok error', content: `${url} -> ${JSON.stringify(res)}`}];
-    }
-    return res.json();
-  } catch (e) {
-    console.log(`ERROR: fetch ${url}:`, e);
-    return [{id: 2, title: 'error', content: `${url} -> ${JSON.stringify(e)}`}];
+  // let url = `https://localhost:3000/api/getPosts`;
+  const res = await fetch(url);
+  if(!res.ok) {
+    console.log(res)
   }
-    return [{id: 3, title: 'wtf', content: ``}];
+  return res.json();
 }
 
 export default async function Home() {
