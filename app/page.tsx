0484ea733f93e9e4f2d8334b301aa9ddeba49aa0
Cormac Pujals <110591827+cormacpujals@users.noreply.github.com>
2023-02-19 23:04:52 -0800

    deployment config test

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,17 @@ import Form from "./Form";
 async function getPosts() {
   const url = `https://${process.env.VERCEL_URL}/api/getPosts`;
   //const url = `https://fullstack-next-prisma-tailwind-nvcsxgraj-cormacpujals.vercel.app/api/getPosts`;
-  const res = await fetch(url);
+  try {
+    const res = await fetch(url);
+  } catch (e) {
+   console.log(`ERROR: fetch ${url}:`, e);
+  return [{"id":1,"title":"Cats","content":"Cats are the best ","published":false},{"id":3,"title":"Dogs","content":null,"published":false},{"id":4,"title":"test","content":null,"published":false},{"id":5,"title":"working?","content":null,"published":false},{"id":6,"title":"test","content":null,"published":false},{"id":7,"title":"another test 4 u","content":null,"published":false}];
+  }
   // if(!res.ok) {
   //  console.log(`ERROR: fetch ${url} -> ${res}`);
   // }
-   console.log(`ERROR: fetch ${url} -> ${res}`);
   //return res.json();
-  return Promise.resolve([{"id":1,"title":"Cats","content":"Cats are the best ","published":false},{"id":3,"title":"Dogs","content":null,"published":false},{"id":4,"title":"test","content":null,"published":false},{"id":5,"title":"working?","content":null,"published":false},{"id":6,"title":"test","content":null,"published":false},{"id":7,"title":"another test 4 u","content":null,"published":false}]);
+  return [];
 }
 
 export default async function Home() {
