ef084d1e579cd9859c3effbaec4008ba477cfdfc
Cormac Pujals <110591827+cormacpujals@users.noreply.github.com>
2023-02-19 23:22:33 -0800

    deployment config test

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Form from "./Form";
 
 // return [{"id":1,"title":"Cats","content":"Cats are the best ","published":false},{"id":3,"title":"Dogs","content":null,"published":false},{"id":4,"title":"test","content":null,"published":false},{"id":5,"title":"working?","content":null,"published":false},{"id":6,"title":"test","content":null,"published":false},{"id":7,"title":"another test 4 u","content":null,"published":false}];
 async function getPosts() {
+  return Promise.resolve([]);
+  // FU VERCEL
+
   const url = `https://${process.env.VERCEL_URL}/api/getPosts`;
   //const url = `https://fullstack-next-prisma-tailwind.vercel.app/api/getPosts`;
   try {
